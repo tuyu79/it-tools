d39deb1dc7a9b3485fa7cdb5fbe77f8571ddc0d3
abel <593051107@qq.com>
2025-04-23 21:32:25 +0800

    register shortcut

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,41 @@
+use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+
+            let ctrl_n_shortcut = Shortcut::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyN);
+            #[cfg(desktop)]
+            app.handle().plugin(
+                tauri_plugin_global_shortcut::Builder::new()
+                    .with_handler(move |_app, shortcut, event| {
+                        println!("{:?}", shortcut);
+                        if shortcut == &ctrl_n_shortcut {
+                            match event.state() {
+                                ShortcutState::Pressed => {
+                                    println!("Ctrl-N Pressed!");
+                                }
+                                ShortcutState::Released => {
+                                    println!("Ctrl-N Released!");
+                                }
+                            }
+                        }
+                    })
+                    .build(),
+            )?;
+
+            app.global_shortcut().register(ctrl_n_shortcut)?;
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![]) // 可以添加其他命令
+        .run(tauri::generate_context!())
+        .expect("Run Tauri App Failed");
 }
